7a450cf69cb508fd91f5b6c30b2d43e5d6a1c97f
Lisa Mtui <lisamtui@gmail.com>
2023-09-13 16:56:28 -0400

    changed utils

--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
 class Utils:
-    def reversed(number):
-        str(number)[::-1]
-        return int(number)
+    @staticmethod
+    def reversed(num):
+        if not isinstance(num, int):
+            raise TypeError("Input must be an integer, not a float")
+        num_str = str(num)
+        reversed_str = num_str[::-1]
+        return int(reversed_str)  # Convert the reversed string back to an integer
 
-    def formatter(number):
-        binary =  (bin(number)[2:])
-        octal =  (oct(number)[2:])
-        return binary, octal
+    @staticmethod
+    def formatter(num):
+        if not isinstance(num, int):
+            raise TypeError("Input must be an integer, not a float")
+        binary_value = bin(num)[2:]
+        octal_value = oct(num)
+        return {"binary": binary_value, "octal": octal_value}
--- utils_tests.py
+++ utils_tests.py
@@ -1,17 +1,21 @@
 import unittest
 from utils import Utils
 
-class TestUtils(unittest.TestCase):
+class Test_Utils(unittest.TestCase):
 
     def test_reversed(self):
         self.assertEqual(Utils.reversed(123), 321)
         self.assertEqual(Utils.reversed(556), 655)
-        self.assertEqual(Utils.reversed("abc"), "cba")
+        with self.assertRaises(TypeError):
+            Utils.reversed("abc")  # Check for TypeError when passing a string
 
     def test_formatter(self):
-        self.assertEqual(Utils.formatter(42), {"binary": "101010", "octal": "52"})
-        self.assertEqual(Utils.formatter(7.5), {"binary": "111.1", "octal": "7.4"})
-        self.assertEqual(Utils.formatter("abc"), {"binary": "110000110110001011010011", "octal": "30306123"})
+        self.assertEqual(Utils.formatter(42), {"binary": "101010", "octal": "0o52"})
+        self.assertEqual(Utils.formatter(7), {"binary": "111", "octal": "0o7"})
+        with self.assertRaises(TypeError):
+            Utils.formatter(7.5)  # Check for TypeError when passing a float
+        with self.assertRaises(TypeError):
+            Utils.formatter("abc")  # Check for TypeError when passing a string
 
 if __name__ == '__main__':
     unittest.main()
